de74a6b57575ead595e20c9d2b1878c3cef01a20
laluRiascos <110619768+laluRiascos@users.noreply.github.com>
2023-10-12 02:26:42 -0500

    Corrección_Completa_05

--- src/Challenge_04/GifGrid.jsx
+++
@@ -1,9 +0,0 @@
-export const GifGrid = ({category}) => {
-
-    console.log(category)
-    return(
-        <>
-            <h3> {category} </h3>
-        </>
-    )
-}
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import FirstApp from './Challenge_02/FirstApp.jsx'
-import Buttons from './Challenge_03/buttons.jsx'
-import GiftExpertApp from './Challenge_04/GiftExpertApp.jsx'
 import GiftExpertAppB from './Challenge_05/GiftExpertAppB.jsx'
 import './index.css'
 
