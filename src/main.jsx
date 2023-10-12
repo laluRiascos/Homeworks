11ad1d8e39d72cb019ccffa70f09e1d91e47f7a0
laluRiascos <110619768+laluRiascos@users.noreply.github.com>
2023-10-12 02:21:12 -0500

    Corrección_Completa_02

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import FirstApp from './Challenge_01/FirstApp.jsx'
+import FirstApp from './Challenge_02/FirstApp.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
