362553740967a1bf954dea6133bef35109243772
laluRiascos <110619768+laluRiascos@users.noreply.github.com>
2023-10-12 02:23:05 -0500

    Corrección_Completa_03

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import FirstApp from './Challenge_02/FirstApp.jsx'
 import Buttons from './Challenge_03/buttons.jsx'
 import './index.css'
 
