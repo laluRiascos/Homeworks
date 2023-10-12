8a0dab1dadb10c7dc761fdc9ce690c51b11958bf
laluRiascos <110619768+laluRiascos@users.noreply.github.com>
2023-10-12 12:04:18 -0500

    Avance_A_09

---
+++ src/Challenge_09/intro-reducer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { useReducer } from 'react'
+import { TodoReducer } from './TodoReducer'
+
+const initialState = [{
+    id: new Date().getTime(),
+    description: 'Hacer los challenges',
+    done: false
+}]
+
+export const TodoApp = () => {
+    const [todos, dispatchTodo] = useReducer(TodoReducer, initialState)
+
+}
