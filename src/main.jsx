fabeac795c0d6ef75fec075d546141768a136657
laluRiascos <110619768+laluRiascos@users.noreply.github.com>
2023-10-12 15:09:18 -0500

    Avance_B_09

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './Challenge_09/TodoApp.jsx'
+import {TodoApp} from './Challenge_09/TodoApp.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
