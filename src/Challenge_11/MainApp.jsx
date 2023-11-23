f2ed08f2009e7ddea7c47229abbe7c660bd37c93
laluRiascos <110619768+laluRiascos@users.noreply.github.com>
2023-11-22 23:48:07 -0500

    Corrección_Complete_11

--- src/Challenge_11/MainApp.jsx
+++ src/Challenge_11/MainApp.jsx
@@ -1,27 +1,26 @@
 import React from 'react';
-import {Link, Navigate, Route, Routes} from 'react-router-dom'
-import AboutPage from './AboutPage'
-import HomePage from './HomePage'
-import LoginPage from './LoginPage'
+import { Navigate } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
+import { AboutPage } from './AboutPage';
+import { HomePage } from './HomePage';
+import { LoginPage } from './LoginPage';
+import { NavComponent } from './NavComponent';
 
 export const MainApp = () => {
-    return (
-        <>
-            <h1> MainApp </h1>
-            <hr/>
+  return (
+    <>
+      <h1>MainApp</h1>
+      <hr />
 
-            <NavLink className={(args) => `${args.isActive? 'active': ''}`} to="/">Home</NavLink>
-            
-            <Link to="/"> Home </Link>
-            <Link to="about"> About </Link>
-            <Link to="login"> Login </Link>
+      {/* Use the NavComponent */}
+      <NavComponent />
 
-            <Routes>
-                <Route path="/" element={<HomePage/>} />
-                <Route path="login" element={<LoginPage/>} />
-                <Route path="about" element={<AboutPage/>} />
-                <Route path="/*" element={<Navigate to="/about"/>} />
-            </Routes>
-        </>
-    )
-}
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/*" element={<Navigate to="/about" />} />
+      </Routes>
+    </>
+  );
+};
